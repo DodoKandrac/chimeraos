d6a1a801ad1fd2ba4bb42b44689bcee08288ab36
Jozef Kandrac <>
2025-08-22 10:01:41 +0200

    Revert "Ensure pacman keyring initialization and cleanup across builds"
    This reverts commit 396a5804b85905e93ab2c4d40ea1ee96f4ec8761.

--- pkgs/build-package.sh
+++ pkgs/build-package.sh
@@ -2,13 +2,6 @@
 
 set -x
 
-# Prepare pacman keyring (idempotent) to avoid PGP trust errors in CI
-sudo bash -c 'command -v timedatectl >/dev/null 2>&1 && timedatectl set-ntp true || true'
-sudo pacman-key --init || true
-sudo pacman-key --populate archlinux || true
-sudo pacman -Sy --needed --noconfirm archlinux-keyring
-sudo pacman -Scc --noconfirm || true
-
 sudo chown -R build:build /workdir/pkgs
 
 PIKAUR_CMD="PKGDEST=/workdir/pkgs pikaur --noconfirm --build-gpgdir /etc/pacman.d/gnupg -S -P /workdir/${1}/PKGBUILD"
